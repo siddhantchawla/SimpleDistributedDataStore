8506e5c164dd6b014cab038e5edebca9a2b3a555
Siddhant Chawla <siddhant.chawla9@gmail.com>
2025-03-10 19:44:55 +0000

    .

--- .devcontainer/start_etcd.sh
+++ .devcontainer/start_etcd.sh
@@ -1,3 +1,6 @@
-# Create the start_etcd.sh script
-echo '#!/bin/bash
-etcd --listen-client-urls http://127.0.0.1:2379 --advertise-client-urls http://127.0.0.1:2379' > .devcontainer/start_etcd.sh
+wget https://github.com/etcd-io/etcd/releases/download/v3.5.12/etcd-v3.5.12-linux-amd64.tar.gz -O etcd.tar.gz
+
+tar xzvf etcd.tar.gz
+sudo mv etcd-v3.5.12-linux-amd64/etcd etcd-v3.5.12-linux-amd64/etcdctl /usr/local/bin/
+
+etcd --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://0.0.0.0:2379
